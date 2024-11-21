1ac105705d74fec868fcdc177f46f960ab1c6978
Mikhail2048 <mikhailpolivakha@gmail.com>
2024-11-21 21:23:57 +0300

    Added test for EndOfLineOptionVerifier

--- src/main/java/io/mpolivaha/maven/plugin/editorconfig/verifiers/impl/EndOfLineOptionVerifier.java
+++ src/main/java/io/mpolivaha/maven/plugin/editorconfig/verifiers/impl/EndOfLineOptionVerifier.java
@@ -1,15 +1,12 @@
 package io.mpolivaha.maven.plugin.editorconfig.verifiers.impl;
 
 import io.mpolivaha.maven.plugin.editorconfig.Editorconfig.Section;
-import io.mpolivaha.maven.plugin.editorconfig.common.BufferedInputStream;
 import io.mpolivaha.maven.plugin.editorconfig.common.ByteArrayLine;
 import io.mpolivaha.maven.plugin.editorconfig.common.TripleFunction;
 import io.mpolivaha.maven.plugin.editorconfig.model.EndOfLine;
 import io.mpolivaha.maven.plugin.editorconfig.model.Option;
 import io.mpolivaha.maven.plugin.editorconfig.verifiers.OptionValidationResult;
 import io.mpolivaha.maven.plugin.editorconfig.verifiers.SpecOptionVerifier;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Map;
 import java.util.Objects;
 
@@ -31,11 +28,13 @@ public class EndOfLineOptionVerifier extends SpecOptionVerifier<EndOfLine> {
 
   @Override
   protected void forEachLine(ByteArrayLine line, int lineNumber, EndOfLine optionValue, OptionValidationResult result, Map<String, Object> executionContext) {
-    if (!Objects.equals(optionValue, line.getEndOfLine())) {
-      // here, we're potentially checking the last line which might be EOF
-      // In this case, we'll 100% will fail the build, and I think it is the
-      // right way to do this since the last line of the file is still a POSIX line
-      result.addErrorMessage(ERROR_MESSAGE_PRODUCER.apply(lineNumber, optionValue.getEolSymbol(), line.getEndOfLine().getEolSymbol()));
+    if (!line.isLastEmptyEOFLine()) {
+      if (!Objects.equals(optionValue, line.getEndOfLine())) {
+        // here, we're potentially checking the last line which might be EOF
+        // In this case, we'll 100% will fail the build, and I think it is the
+        // right way to do this since the last line of the file is still a POSIX line
+        result.addErrorMessage(ERROR_MESSAGE_PRODUCER.apply(lineNumber, optionValue.getEolSymbol(), line.getEndOfLine().getEolSymbol()));
+      }
     }
   }
 
