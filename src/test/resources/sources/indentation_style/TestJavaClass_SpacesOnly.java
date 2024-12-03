5c8a705a4c9dffac82c35f3e2517be9f43960bbc
mipo256 <mikhailpolivakha@gmail.com>
2024-12-03 22:23:33 +0300

    Added TestJavaClass_TabsOnly.java

---
+++ src/test/resources/sources/indentation_style/TestJavaClass_SpacesOnly.java
@@ -0,0 +1,18 @@
+package com.something.other;
+
+public class TestJavaClass_SpacesOnly {
+
+  public static final String PARAM = "PARAM";
+  public static void main(String[] args) {
+    System.out.println("Hey!");
+
+    String property = System
+        .getProperties()
+        .getProperty(PARAM);
+
+    if (property == null)
+      return;
+
+    System.out.println(property);
+  }
+}
