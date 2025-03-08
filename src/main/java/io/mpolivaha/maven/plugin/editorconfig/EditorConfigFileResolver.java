5e3b6c45cc21f7ec94f4d73b0e67772c3c7e7fd2
mipo256 <mikhailpolivakha@gmail.com>
2025-03-08 22:55:32 +0300

    Implemented EditorConfigFileResolver

--- src/main/java/io/mpolivaha/maven/plugin/editorconfig/EditorConfigFileResolver.java
+++ src/main/java/io/mpolivaha/maven/plugin/editorconfig/EditorConfigFileResolver.java
@@ -1,7 +1,9 @@
 package io.mpolivaha.maven.plugin.editorconfig;
 
-import java.io.InputStream;
+import java.io.File;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 import org.apache.maven.project.MavenProject;
 
@@ -10,15 +12,20 @@ import org.apache.maven.project.MavenProject;
  *
  * @author Mikhail Polivakha
  */
-public class RootEditorConfigFileResolver {
+public class EditorConfigFileResolver {
 
   /**
+   * Resolves {@code .editorconfig} file from relative path. The relative path is assumed to
+   * be relative to the {@link MavenProject#getBasedir()} Maven Project basedir}
    *
    * @param editorconfig - user specified .editorconfig location
    * @return Optional Path of editorconfig root file. {@link Optional#empty() empty optional} in case file is not found
-   * TODO: implement
    */
   public Optional<Path> findRootEditorConfig(MavenProject mavenProject, String editorconfig) {
-    throw new UnsupportedOperationException();
+      File basedir = mavenProject.getBasedir();
+
+      return Optional
+        .of(Paths.get(basedir.getAbsolutePath(), editorconfig))
+        .filter(Files::exists);
   }
 }
