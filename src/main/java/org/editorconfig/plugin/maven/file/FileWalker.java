857195b4c2dd3dc77c752c614c2bec2ffec4263f
mipo256 <mikhailpolivakha@gmail.com>
2025-05-08 22:39:55 +0300

    Added BFS File Walker API

--- src/main/java/org/editorconfig/plugin/maven/file/FileWalker.java
+++ src/main/java/org/editorconfig/plugin/maven/file/FileWalker.java
@@ -4,84 +4,74 @@
  */
 package org.editorconfig.plugin.maven.file;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileVisitOption;
-import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
-import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
-import org.apache.maven.plugin.logging.Log;
-import org.editorconfig.plugin.maven.config.PluginConfiguration;
-import org.editorconfig.plugin.maven.utils.ExceptionUtils;
-import org.editorconfig.plugin.maven.utils.ExecutionUtils;
-
+/**
+ * This class exists mainly because the {@link java.nio.file.Files#walkFileTree(Path, Set, int, FileVisitor)} and similar APIs
+ * traverse the directory using DFS. This is
+ */
 public class FileWalker {
 
-    private static final BiFunction<String, Exception, String> DIRECTORY_VISIT_ERROR =
-            (path, error) ->
-                    "Error during walking the file-tree. Unable to fully traverse directory '%s'. Exception: \n%s"
-                            .formatted(path, ExceptionUtils.getStackTrace(error));
-
-    private static final BiFunction<String, Exception, String> FILE_INSPECTION_ERROR =
-            (path, error) -> "Error during inspecting the file : '%s'. Exception: \n%s"
-                    .formatted(path, ExceptionUtils.getStackTrace(error));
-
-    public void walkRecursiveFilesInDirectory(Path root, Consumer<Path> contentConsumer)
+    public void walkRecursiveBFS(Path root, Consumer<Path> contentConsumer)
             throws IOException {
-        Files.walkFileTree(
-                root, Set.of(FileVisitOption.FOLLOW_LINKS), Integer.MAX_VALUE, new FileVisitor<>() {
+        var queue = new LinkedList<Path>();
+        queue.add(root);
+        this.walkRecursiveBFS(queue, contentConsumer);
+    }
 
-                    @Override
-                    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
-                        return null;
-                    }
+    /**
+     * Walks the files in the given directory using bread-first search.
+     * Consider the following file structure:
+     * <p>
+     * <pre>
+     * ├── file1
+     * ├── file2
+     * ├── file3
+     * ├── l1
+     * │    ├── l1_file1
+     * │    ├── l1_file2
+     * │    └── l2
+     * │        ├── l2_file1
+     * │        └── l3
+     * └── v2
+     *     └── v2_file1
+     * </pre>
+     * In the files would be processed in the following order:
+     * <ol>
+     *     <li>Files in the root dir (file1, file2, file3)</li>
+     *     <li>Files in the inner dirs, either l1 or v2</li>
+     *     <li>Files in the inner dirs, either l1 or v2</li>
+     *     <li>Files in the inner dirs, either l2</li>
+     * </ol>
+     */
+    private void walkRecursiveBFS(Queue<Path> paths, Consumer<Path> contentConsumer) {
+        Path currentFile = paths.poll();
 
-                    @Override
-                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
-                            throws IOException {
-                        try {
-                            contentConsumer.accept(file);
-                        } catch (Throwable t) {
-                            return FileVisitResult.TERMINATE;
-                        }
-                        return FileVisitResult.CONTINUE;
-                    }
+        if (Files.isDirectory(currentFile)) {
+            File[] files = currentFile.toFile().listFiles();
 
-                    @Override
-                    public FileVisitResult visitFileFailed(Path file, IOException exc)
-                            throws IOException {
-                        if (exc != null) {
-                            if (file.toFile().isDirectory()) {
-                                ExecutionUtils.handleError(DIRECTORY_VISIT_ERROR.apply(
-                                        file.toFile().getAbsolutePath(), exc));
-                            } else {
-                                ExecutionUtils.handleError(FILE_INSPECTION_ERROR.apply(
-                                        file.toFile().getAbsolutePath(), exc));
-                            }
-                        }
-                        return FileVisitResult.CONTINUE;
-                    }
+            Queue<Path> subQueue = new LinkedList<>();
 
-                    @Override
-                    public FileVisitResult postVisitDirectory(Path dir, IOException exc)
-                            throws IOException {
-                        if (exc != null) {
-                            // This will fail the execution in case we're in strict mode
-                            ExecutionUtils.handleError(DIRECTORY_VISIT_ERROR.apply(
-                                    dir.toFile().getAbsolutePath(), exc));
-                        } else {
-                            PluginConfiguration.getInstance()
-                                    .<Log>getLog()
-                                    .debug(
-                                            "The directory '%s' and all recursive files have been investigated successfully");
-                        }
-                        return FileVisitResult.CONTINUE;
-                    }
-                });
+            for (var child : files) {
+                if (child.isFile()) {
+                    contentConsumer.accept(child.toPath());
+                } else if (child.isDirectory()) {
+                    subQueue.add(child.toPath());
+                }
+            }
+
+            paths.addAll(subQueue);
+            this.walkRecursiveBFS(paths, contentConsumer);
+        } else {
+            contentConsumer.accept(currentFile);
+        }
     }
 }
