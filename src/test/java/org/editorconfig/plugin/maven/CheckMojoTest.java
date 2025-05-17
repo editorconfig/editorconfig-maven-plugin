67b595d2cc5c33b6acee4340301a7162947100c0
mipo256 <mikhailpolivakha@gmail.com>
2025-05-17 14:22:52 +0300

    Added E2E test

--- src/test/java/org/editorconfig/plugin/maven/CheckMojoTest.java
+++ src/test/java/org/editorconfig/plugin/maven/CheckMojoTest.java
@@ -4,26 +4,32 @@
  */
 package org.editorconfig.plugin.maven;
 
+import static org.editorconfig.plugin.maven.utils.ExecutionUtils.executeExceptionally;
+import static org.editorconfig.plugin.maven.utils.ExecutionUtils.mapExceptionally;
+
 import java.io.File;
 import java.lang.reflect.Field;
 import java.net.URISyntaxException;
 
-import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
-import org.junit.jupiter.api.Disabled;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.editorconfig.plugin.maven.utils.ExecutionUtils.executeExceptionally;
-import static org.editorconfig.plugin.maven.utils.ExecutionUtils.mapExceptionally;
-
-@Disabled
 class CheckMojoTest {
 
     @Test
-    void test() throws MojoExecutionException, MojoFailureException, URISyntaxException {
-        instantiateMojo(testFile("e2e/single-root-config/pom.xml"), ".editorconfig")
-                .execute();
+    void testE2EScenario() {
+        Assertions
+          .assertThatThrownBy(
+            () -> instantiateMojo(testFile("e2e/single-root-config/pom.xml"), ".editorconfig").execute())
+          .isInstanceOf(MojoFailureException.class)
+          .hasMessageContainingAll(
+            "In current project found 3 files that violate some of the .editorconfig rules",
+            "pom.xml' in total encountered 1 violations", // indentation
+            "MyEntity.java' in total encountered 2 violations", // indentation
+            "config.yaml' in total encountered 2 violations" // cr instead of lf
+          );
     }
 
     private static File testFile(String testFile) throws URISyntaxException {
